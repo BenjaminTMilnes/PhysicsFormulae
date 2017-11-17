4664dd7b12c2d05700d374a3f681547668ced60a
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-11-17 22:37:15 +0000

    Setup AngularJS web app

---
+++ PhysicsFormulae.WebApplication/application.js
@@ -0,0 +1,28 @@
+﻿var application = angular.module("PhysicsFormulae", []);
+
+application.directive("katex", function () {
+    return {
+        restrict: "E",
+        link: function (scope, element, attributes) {
+            var latex = attributes.latex;
+            if (typeof (katex) === "undefined") {
+                require(["katex"], function (katex) {
+                    katex.render(latex, element[0]);
+                });
+            }
+            else {
+                katex.render(latex, element[0]);
+            }
+        }
+    }
+});
+
+application.controller("SearchController", ["$scope", "$http", function SearchController($scope, $http) {
+
+    $scope.formulae = [];
+
+    $http.get("formulae.json").then(function (response) {
+        $scope.formulae = response.data;
+    });
+
+}])
