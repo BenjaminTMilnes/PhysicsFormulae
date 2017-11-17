f0411f8ba6d43023aa6fc5873a6baee5cc7f3d58
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-11-17 21:51:45 +0000

    Made the compiler terminal program

--- PhysicsFormulae.TerminalApplication/Program.cs
+++ PhysicsFormulae.TerminalApplication/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using PhysicsFormulae.Compiler;
+using Newtonsoft.Json;
 
 namespace PhysicsFormulae.TerminalApplication
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            var compiler = new Compiler.Compiler();
+            var formulae = new List<Formula>();
+
+            var directoryInfo = new DirectoryInfo(@"..\..\..\PhysicsFormulae.Formulae");
+            var files = directoryInfo.GetFiles("*.formula");
+
+            foreach (var file in files)
+            {
+                var lines = File.ReadAllLines(file.FullName);
+                var formula = compiler.CompileFormula(lines);
+                formulae.Add(formula);
+
+                Console.WriteLine(formula.Reference);
+            }
+
+            var serializer = new JsonSerializer();
+
+            using (var streamWriter = new StreamWriter(@"..\..\..\PhysicsFormulae.Formulae\Compiled.json"))
+            using (var jsonTextWriter = new JsonTextWriter(streamWriter))
+            {
+                serializer.Serialize(jsonTextWriter, formulae);
+            }
+
+            Console.ReadLine();
         }
     }
 }
