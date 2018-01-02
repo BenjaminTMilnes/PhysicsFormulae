cdbb10fa350a74330e4aa1b04d565e42191981c5
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2018-01-02 17:29:10 +0000

    Added BibTeX for the webpage, improved the shared data service, and eliminated errors caused by the formula not having loaded

--- PhysicsFormulae.WebApplication/application.js
+++ PhysicsFormulae.WebApplication/application.js
@@ -70,52 +70,65 @@ application.filter("searchFormulae", function () {
     }
 });
 
-application.factory("formulae", ["$http", function ($http) {
-    var formulae = { data: [] };
+application.factory("dataService", ["$http", function ($http) {
+    var dataService = {
+        getData: function () {
+            return $http.get("formulae.json").then(function (response) {
+                return response.data;
+            });
+        }
+    };
 
-    $http.get("formulae.json").then(function (response) {
-        formulae.data = response.data;
-    });
-
-    return formulae;
+    return dataService;
 }]);
 
-application.controller("SearchController", ["$scope", "formulae", function SearchController($scope, formulae) {
+application.controller("SearchController", ["$scope", "dataService", function SearchController($scope, dataService) {
 
-    $scope.formulae = formulae;
+    dataService.getData().then(function (data) {
+        $scope.formulae = data;
+    });
 
 }]);
 
-application.controller("FormulaController", ["$scope", "$routeParams", "formulae", function FormulaController($scope, $routeParams, formulae) {
+application.controller("FormulaController", ["$scope", "$routeParams", "dataService", function FormulaController($scope, $routeParams, dataService) {
 
-    $scope.formulae = formulae;
+    $scope.formulae = null;
 
-    $scope.$watch("formulae.data", function () {
-        $scope.showFormula();
-    });
-    
-    $scope.showFormula = function () {
-        for (var i = 0; i < $scope.formulae.data.length; i++) {
-            if ($scope.formulae.data[i].Reference == $routeParams.reference) {
-                $scope.formula = $scope.formulae.data[i];
+    dataService.getData().then(function (data) {
+        $scope.formulae = data;
+
+        for (var i = 0; i < $scope.formulae.length; i++) {
+            var formula = $scope.formulae[i];
+
+            if (formula.Reference == $routeParams.reference) {
+                $scope.formula = formula;
             }
         }
-    }
-    
+    });
+
     $scope.getFormulaContent = function () {
+        if (!$scope.formula) {
+            return "";  
+        }
+
         return "<katex latex=\"\\displaystyle " + $scope.formula.Content + "\"></katex>";
     }
 
     $scope.replaceMathematicsMarkers = function (text) {
+        if (!text) {
+            return "";
+        }
 
         var re = /\$(.+?)\$/gi;
         var textWithKaTeX = text.replace(re, "<katex latex=\"$1\"></katex>")
 
         return textWithKaTeX;
-
     }
 
     $scope.getLaTeXForEntireFormula = function () {
+        if (!$scope.formula) {
+            return "";
+        }
 
         var latex = "";
 
@@ -147,7 +160,56 @@ application.controller("FormulaController", ["$scope", "$routeParams", "formulae
 
     }
 
+    $scope.getBibTeXForThisWebpage = function () {
+        if (!$scope.formula) {
+            return "";
+        }
+
+        var bibtex = "";
+        var database = new BibTeXDatabase();
+        var webpage = new BibTeXWebpage();
+
+        webpage.citationKey = "PhysicsFormulae_" + $scope.formula.Reference;
+        webpage.author.value = "B. T. Milnes";
+        webpage.title.value = $scope.formula.Title;
+        webpage.websiteTitle.value = "Physics Formulae";
+        webpage.url.value = "http://www.physicsformulae.com/#/formula/" + $scope.formula.Reference;
+        webpage.dateAccessed.value = $scope.getTodaysDate();
+
+        database.entries.push(webpage);
+
+        var exporter = new BibTeXExporter();
+
+        bibtex = exporter.convertBibTeXDatabaseToText(database).trim();
+
+        return bibtex;
+    }
+
+    $scope.getTodaysDate = function () {
+        var today = new Date();
+
+        var day = today.getDate();
+        var month = today.getMonth() + 1;
+        var year = today.getFullYear();
+
+        if (day < 10) {
+            day = "0" + day;
+        }
+
+        if (month < 10) {
+            month = "0" + month;
+        }
+
+        var todaysDate = year + "/" + month + "/" + day;
+
+        return todaysDate;
+    }
+
     $scope.getBibTeXForOriginalReferences = function () {
+        if (!$scope.formula) {
+            return "";
+        }
+
         var bibtex = "";
 
         var database = new BibTeXDatabase();
@@ -157,7 +219,7 @@ application.controller("FormulaController", ["$scope", "$routeParams", "formulae
 
             if (reference.Type == "Book") {
                 var book = new BibTeXBook();
-                book.citationKey = "r" + i;
+                book.citationKey = $scope.getCitationKeyForReference(reference);
                 book.title.value = reference.Title;
                 book.author.value = $scope.getAuthorsString(reference.Authors);
                 book.publisher.value = reference.Publisher;
@@ -168,10 +230,20 @@ application.controller("FormulaController", ["$scope", "$routeParams", "formulae
 
         var exporter = new BibTeXExporter();
 
-        bibtex = exporter.convertBibTeXDatabaseToText(database);
+        bibtex = exporter.convertBibTeXDatabaseToText(database).trim();
 
         return bibtex;
 
+    }
+
+    $scope.getCitationKeyForReference = function (reference) {
+
+        var citationKey = "";
+
+        citationKey = reference.Authors[0];
+        citationKey = citationKey.replace(/[\s\.]/g, "");
+
+        return citationKey;
     }
 
     $scope.getAuthorsString = function (authors) {
@@ -182,7 +254,7 @@ application.controller("FormulaController", ["$scope", "$routeParams", "formulae
             if (i > 0 && authors.length > 2) {
                 authorsString += ", ";
             }
-            else if (authors.length <= 2) {
+            else if (i > 0 && authors.length == 2) {
                 authorsString += " ";
             }
             if (i == authors.length - 1) {
