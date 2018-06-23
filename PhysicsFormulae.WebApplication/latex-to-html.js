4dd301d9a537aa11ae2e0d6fe425110a02b32339
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2018-06-23 11:28:11 +0100

    Began creating more robust LaTeX to HTML conversion algorithm

---
+++ PhysicsFormulae.WebApplication/latex-to-html.js
@@ -0,0 +1,71 @@
+﻿class Subscript {
+    constructor() {
+        this.type = "subscript";
+        this.content = {};
+    }
+}
+
+class Superscript {
+    constructor() {
+        this.type ="superscript";
+        this.content = {};
+    }
+}
+
+class Bold {
+    constructor() {
+        this.type = "bold";
+        this.content = {};
+    }
+}
+
+class RomanText {
+    constructor() {
+        this.type = "romantext";
+        this.content = {};
+    }
+}
+
+class Symbol {
+    constructor() {
+        this.type = "symbol";
+        this.latex = "";
+        this.htmlEntity = "";
+    }
+}
+
+class Text {
+    constructor() {
+        this.type = "text";
+        this.content = "";
+    }
+}
+
+
+function getMathsObjectFromLaTeX(latex) {
+
+}
+
+function convertMathsObjectToHTML(mathsObject) {
+
+    if (mathsObject.type == "subscript") {
+        return "<sub>" + convertMathsObjectToHTML(mathsObject.content) + "</sub>";
+    }
+
+    if (mathsObject.type == "superscript") {
+        return "<sup>" + convertMathsObjectToHTML(mathsObject.content) + "</sup>";
+    }
+
+    if (mathsObject.type == "bold") {
+        return "<strong>" + convertMathsObjectToHTML(mathsObject.content) + "</strong>";
+    }
+
+    if (mathsObject.type == "romantext") {
+        return "<span style='font-style: normal;'>" + convertMathsObjectToHTML(mathsObject.content) + "</span>";
+    }
+
+    if (mathsObject.type == "text") {
+        return mathsObject.content;
+    }
+
+}
