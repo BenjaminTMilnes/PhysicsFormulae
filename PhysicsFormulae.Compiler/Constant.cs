b7c7d910175a75be7b0d7d52d942b60bb09f55f3
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2018-06-29 13:17:54 +0100

    Added core classes for compiling constants

---
+++ PhysicsFormulae.Compiler/Constant.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace PhysicsFormulae.Compiler
+{
+    public class Constant
+    {
+        public string Reference { get; set; }
+        public string Title { get; set; }
+        public string Interpretation { get; set; }
+        public ConstantType Type { get; set; }
+        public string Symbol { get; set; }
+        public IList<Value> Values { get; set; }
+        public IList<Reference> References { get; set; }
+        public IList<SeeMoreLink> SeeMore { get; set; }
+        public IList<string> Tags { get; set; }
+
+        public Constant()
+        {
+            Values = new List<Value>();
+            References = new List<Reference>();
+            SeeMore = new List<SeeMoreLink>();
+            Tags = new List<string>();
+        }
+    }
+}
---
+++ PhysicsFormulae.Compiler/Value.cs
@@ -0,0 +1,9 @@
+﻿namespace PhysicsFormulae.Compiler
+{
+    public class Value
+    {
+        public string Coefficient { get; set; }
+        public string Exponent { get; set; }
+        public string Units { get; set; }
+    }
+}
