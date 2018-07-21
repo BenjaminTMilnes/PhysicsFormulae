5a949511cd038868b755836512ffd43ecd009d97
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2018-07-21 10:15:01 +0100

    Added first unit test

---
+++ PhysicsFormulae.Compiler.Tests/AutotaggerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PhysicsFormulae.Compiler;
+
+namespace PhysicsFormulae.Compiler.Tests
+{
+    [TestFixture]
+    public class AutotaggerTests
+    {
+        protected Autotagger _autotagger { get; set; }
+
+        public AutotaggerTests()
+        {
+            _autotagger = new Autotagger();
+        }
+
+        [TestCase("where  is the distance", "where $x$ is the distance")]
+        public void TestRemoveLaTeX(string normalised, string unnormalised)
+        {
+            Assert.AreEqual(normalised, _autotagger.RemoveLaTeX(unnormalised));
+        }
+    }
+}
--- PhysicsFormulae.Compiler/Autotagger.cs
+++ PhysicsFormulae.Compiler/Autotagger.cs
@@ -11,6 +11,11 @@ namespace PhysicsFormulae.Compiler
     {
         protected IList<string> excludedWords = new List<string>() { "a", "an", "the", "in", "at", "on", "to", "from", "under", "over", "above", "below", "with", "without", "of", "it", "is", "are", "be", "which", "when", "what", "who", "how", "why", "where", "something", "through", "for", "along", "there", "must", "towards", "that", "and", "its", "know", "known", "travel", "fast", "faster", "than", "this", "carry", "carried", "by", "use", "used", "description", "describe", "described", "number", "include", "including", "move", "moving", "speeds", "or", "respect", "equal", "minus", "one", "multiply", "multiplied", "travel", "travelled", "produce", "produced", "directly", "direct", "left", "right", "hand", "side", "open", "closed", "per", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" };
 
+        public string RemoveLaTeX(string text)
+        {
+            return Regex.Replace(text, @"\$[^\$]*\$", "");
+        }
+
         public IEnumerable<string> GetWords(string text)
         {
             var normalisedText = text;
